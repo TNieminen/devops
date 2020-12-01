bc3e19b641a0a6e43b5b9a533f3a19f5e7068bbc
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-01 13:21:48 +0800

    feat: added support for fanout consumers and producers

--- vh4/src/rabbitmq/index.js
+++ vh4/src/rabbitmq/index.js
@@ -3,12 +3,13 @@
  */
 
 /**
+ * @description creates a connection to rabbitMQ
  * @returns {Promise<Channel>}
  */
-async function createRabbitConnection({rabbitMq, connectionString = '', exchange = ''}) {
+async function createRabbitConnection({rabbitMq, connectionString = '', exchange = '', type = 'topic'}) {
   const connection = await rabbitMq.connect(connectionString)
   const channel = await connection.createChannel()
-  await channel.assertExchange(exchange, 'topic', {
+  await channel.assertExchange(exchange, type, {
     durable: false
   })
   return channel
@@ -19,8 +20,8 @@ async function createRabbitConnection({rabbitMq, connectionString = '', exchange
  * Syntax for connectionString: `amqp://${userName}:${password}@${serverUrl}:${serverPort}`.
  * In some services serverPort is not needed.
  */
-exports.initExchangeProducer = async({rabbitMq, connectionString = '', exchange = ''}) => {
-  return createRabbitConnection({rabbitMq, connectionString, exchange})
+exports.initTopicProducer = async({rabbitMq, connectionString = '', exchange = ''}) => {
+  return createRabbitConnection({rabbitMq, connectionString, exchange, type:'topic'})
 }
 
 /**
@@ -28,10 +29,33 @@ exports.initExchangeProducer = async({rabbitMq, connectionString = '', exchange 
  * Syntax for connectionString: `amqp://${userName}:${password}@${serverUrl}:${serverPort}`.
  * In some services serverPort is not needed.
  */
-exports.initExchangeConsumer = async({rabbitMq, connectionString = '', exchange = '', topic = ''}) => {
-  const channel = await createRabbitConnection({rabbitMq, connectionString, exchange})
+exports.initTopicConsumer = async({rabbitMq, connectionString = '', exchange = '', topic = ''}) => {
+  const channel = await createRabbitConnection({rabbitMq, connectionString, exchange, type:'topic'})
   // empty string so we can assign a random queue automatically
   const q = await channel.assertQueue('', {exclusive: true})
   channel.bindQueue(q.queue, exchange, topic)
+  return {channel, queue:q.queue}
+}
+
+/**
+ * @description Creates a producer instance of rabbitMQ in a given exchange
+ * Syntax for connectionString: `amqp://${userName}:${password}@${serverUrl}:${serverPort}`.
+ * In some services serverPort is not needed.
+ */
+exports.initFanoutProducer = async({rabbitMq, connectionString = '', exchange = ''}) => {
+  return createRabbitConnection({rabbitMq, connectionString, exchange, type:'fanout'})
+}
+
+/**
+ * @description creates a consumer instance that binds a automatically created queue in a given exchange with a given topic.
+ * Syntax for connectionString: `amqp://${userName}:${password}@${serverUrl}:${serverPort}`.
+ * In some services serverPort is not needed.
+ */
+exports.initFanoutConsumer = async({rabbitMq, connectionString = '', exchange = '', topic = ''}) => {
+  const channel = await createRabbitConnection({rabbitMq, connectionString, exchange, type:'topic'})
+  // empty string so we can assign a random queue automatically
+  const q = await channel.assertQueue('', {exclusive: true})
+  // we leave the last option of topic empty so we can broadcast to all queues
+  channel.bindQueue(q.queue, exchange, '')
   return {channel, queue:q.queue}
 }

57b8fa412dc8035fa80b158a28f01e77c9f5fd28
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-02 16:08:49 +0800

    feat: added basic class and queue init tests

--- vh4/src/imed/test.js
+++ vh4/src/imed/test.js
@@ -1,7 +1,26 @@
 const expect = require('expect')
+const Imed = require('./index')
+const sinon = require('sinon')
+const mockQueue = require('@badgrhammer/rabbitmq-helpers/src/mock')
 
 describe('===== IMED =====', () => {
-  it('should pass', async() => {
-    expect(true).toEqual(true)
+  beforeEach(() => {
+    imed = new Imed()
+  })
+
+  describe('==== constructor ====', () => {
+    it('Should create a new instance without errors', async() => {
+      expect(() => new Imed()).not.toThrow()
+    })
+    it('Should set default state to be RUNNING', async() => {
+      expect(imed.state).toEqual('RUNNING')
+    })    
+  })
+  
+  describe('==== initQueue ====', () => {
+    it('Should return mock queue implementation in test env', () => {
+      imed.initQueue()
+      expect(imed.queue).toMatchObject(mockQueue)
+    })
   })
 })

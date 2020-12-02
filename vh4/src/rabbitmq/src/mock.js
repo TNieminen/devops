915a5b68207dd223120d3b8c05382ba0c1124c33
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-02 14:58:29 +0800

    chore: updated rabbitmq helpers mock

--- vh4/src/rabbitmq/src/mock.js
+++ vh4/src/rabbitmq/src/mock.js
@@ -1,6 +1,7 @@
 const {EventEmitter} = require('events')
 
 module.exports = {
+  receiveTopicMessages:true,
   messages:{},
   emitter: new EventEmitter(),
   publishTopicMessage({message, topic}) {
@@ -16,19 +17,33 @@ module.exports = {
       throw new Error('Cannot publish fanout message without message')
     }
   },
-  mockReceivedFanoutMessage({message}) {
-    const {id, payload, timestamp} = JSON.parse(message)
+  /**
+   * @description simulates emits and storing of relevant data to messages 
+   * on a fanout event
+   */
+  mockReceivedFanoutMessage({id, payload, timestamp}) {
     this.messages[id] = {payload,timestamp}
     this.emitter.emit('message', {id,payload,timestamp})
   },
-  mockReceivedTopicMessage({}) {
-    this.emitter.emit('message', {payload,timestamp})
+
+  /**
+   * @description simulated emits on topic messages
+   */
+  mockReceivedTopicMessage({payload, timestamp}) {
+    if (this.receiveTopicMessages) {
+      this.emitter.emit('message', {payload,timestamp}) 
+    }
   },
   getMessageById(id) {
     return this.messages[id]
   },
   on(event,fn) {
     this.emitter.on(event,fn)
+  },
+  startReceivingTopicMessages() {
+    this.receiveTopicMessages = true
+  },
+  stopReceivingTopicMessages() {
+    this.receiveTopicMessages = false
   }
-
-} 
+}

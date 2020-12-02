f5a1164602e5de972c9413aaf2a7a0ac6e368004
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-02 14:35:28 +0800

    chore: added basic empty Obse class

--- vh4/src/obse/index.js
+++ vh4/src/obse/index.js
@@ -1,30 +1,46 @@
 require('dotenv-defaults').config()
+const Queue = require('@badgrhammer/rabbitmq-helpers')
 const pino = require('pino')
-const fs = require('fs')
-const rabbitMq = require('amqplib')
-const {RABBIT_SERVER_URL, RABBIT_SERVER_PORT, RABBIT_USERNAME, RABBIT_PASSWORD, EXCHANGE, ENV, LOGLEVEL} = process.env
-const {initExchangeConsumer} = require('@badgrhammer/rabbitmq-helpers')
-const {appendToFile, deleteFile} = require('./utils')
-const logger = pino({level: LOGLEVEL || 'error'})
-const serverUrl = ENV === 'development' ? 'rabbit' : RABBIT_SERVER_URL
-const serverPort = ENV === 'development' ? `:${RABBIT_SERVER_PORT}` : ''
-const connectionString = `amqp://${RABBIT_USERNAME}:${RABBIT_PASSWORD}@${serverUrl}${serverPort}` 
+const queueMock = require('@badgrhammer/rabbitmq-helpers/src/mock')
+const {RABBIT_SERVER_URL, RABBIT_SERVER_PORT, RABBIT_USERNAME, RABBIT_PASSWORD, TOPIC_EXCHANGE, FANOUT_EXCHANGE, ENV, LOGLEVEL} = process.env
 
-async function start() {
-  await deleteFile()
-  const {channel, queue} = await initExchangeConsumer({
-    rabbitMq,
-    connectionString,
-    topic: 'my.#',
-    exchange: EXCHANGE      
-  })
-  channel.consume(queue, (message) => {
-    if (message !== null) {
-      channel.ack(message)
-      const logMessage = `${new Date().toISOString()} Topic ${message.fields.routingKey}: ${message.content.toString()}`
-      appendToFile(logMessage)
-    }
-  })
+const logger = pino({level: LOGLEVEL || 'error'})
+// TODO: Using docker as env is not consistent with docker setup, this needs to be made consistent
+const serverUrl = ENV === 'docker' ? 'rabbit' : RABBIT_SERVER_URL
+const serverPort = ENV === 'docker' ? `:${RABBIT_SERVER_PORT}` : ''
+const connectionString = `amqp://${RABBIT_USERNAME}:${RABBIT_PASSWORD}@${serverUrl}${serverPort}` 
+const {appendToFile, deleteFile} = require('./utils')
+
+const rabbitConfig = {
+  RABBIT_SERVER_URL, 
+  RABBIT_SERVER_PORT,
+  RABBIT_USERNAME,
+  RABBIT_PASSWORD, 
+  TOPIC_EXCHANGE,
+  FANOUT_EXCHANGE,
+  CONNECTION_STRING:connectionString
 }
 
-start()
+module.exports = class Obse {
+  constructor() {
+  }
+}
+
+// async function start() {
+//   await deleteFile()
+//   const {channel, queue} = await initExchangeConsumer({
+//     rabbitMq,
+//     connectionString,
+//     topic: 'my.#',
+//     exchange: EXCHANGE      
+//   })
+//   channel.consume(queue, (message) => {
+//     if (message !== null) {
+//       channel.ack(message)
+//       const logMessage = `${new Date().toISOString()} Topic ${message.fields.routingKey}: ${message.content.toString()}`
+//       appendToFile(logMessage)
+//     }
+//   })
+// }
+
+// start()

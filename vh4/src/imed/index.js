fc96b54eeb46d45d4ff40f572de918096eec8539
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-02 16:09:08 +0800

    feat: added basic implemenation for imed class

--- vh4/src/imed/index.js
+++ vh4/src/imed/index.js
@@ -1,38 +1,64 @@
 require('dotenv-defaults').config()
-const rabbitMq = require('amqplib')
+const Queue = require('@badgrhammer/rabbitmq-helpers')
 const pino = require('pino')
-const {initExchangeConsumer, initExchangeProducer} = require('@badgrhammer/rabbitmq-helpers')
-const {RABBIT_SERVER_URL, RABBIT_SERVER_PORT, RABBIT_USERNAME, RABBIT_PASSWORD, EXCHANGE, ENV, LOGLEVEL} = process.env
+const queueMock = require('@badgrhammer/rabbitmq-helpers/src/mock')
+const {RABBIT_SERVER_URL, RABBIT_SERVER_PORT, RABBIT_USERNAME, RABBIT_PASSWORD, TOPIC_EXCHANGE, FANOUT_EXCHANGE, ENV, LOGLEVEL} = process.env
 
 const logger = pino({level: LOGLEVEL || 'error'})
 const serverUrl = ENV === 'development' ? 'rabbit' : RABBIT_SERVER_URL
 const serverPort = ENV === 'development' ? `:${RABBIT_SERVER_PORT}` : ''
 const connectionString = `amqp://${RABBIT_USERNAME}:${RABBIT_PASSWORD}@${serverUrl}${serverPort}` 
 
-
-async function start() {
-  const producer = await initExchangeProducer({
-    rabbitMq,
-    connectionString,
-    exchange: EXCHANGE
-  })
-
-  const {channel:consumer, queue} = await initExchangeConsumer({
-    rabbitMq,
-    connectionString,
-    topic: 'my.o',
-    exchange: EXCHANGE
-  })
-  
-  consumer.consume(queue, (message) => {
-    if (message !== null) {
-      setTimeout(() => {
-        const sendMessage = `Got message ${message.content.toString()}`
-        producer.publish(EXCHANGE,'my.i', Buffer.from(sendMessage))
-        consumer.ack(message)
-      },1000)
-    }
-  })
+const rabbitConfig = {
+  RABBIT_SERVER_URL, 
+  RABBIT_SERVER_PORT,
+  RABBIT_USERNAME,
+  RABBIT_PASSWORD, 
+  TOPIC_EXCHANGE,
+  FANOUT_EXCHANGE,
+  CONNECTION_STRING:connectionString
 }
 
-start()
+module.exports = class Imed {
+  constructor() {
+    this.state = 'RUNNING'    
+  }
+  
+  initQueue() {
+    if (ENV === 'test') {
+      this.queue = queueMock
+    }
+    else {
+      this.queue = new Queue({rabbitConfig, topicProducer:true, topicConsumer:{topic:'my.o'}, fanoutConsumer:true})
+    }
+  }
+
+}
+
+
+// async function start() {
+//   const producer = await initExchangeProducer({
+//     rabbitMq,
+//     connectionString,
+//     exchange: EXCHANGE
+//   })
+
+//   const {channel:consumer, queue} = await initExchangeConsumer({
+//     rabbitMq,
+//     connectionString,
+//     topic: 'my.o',
+//     exchange: EXCHANGE
+//   })
+  
+//   consumer.consume(queue, (message) => {
+//     if (message !== null) {
+//       setTimeout(() => {
+//         const sendMessage = `Got message ${message.content.toString()}`
+//         producer.publish(EXCHANGE,'my.i', Buffer.from(sendMessage))
+//         consumer.ack(message)
+//       },1000)
+//     }
+//   })
+// }
+
+// start()

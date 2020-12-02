a917fc319395f1a3b3256a39d833cf373cf743be
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-02 14:38:11 +0800

    feat: added tests for default state RUNNING and queue init

--- vh4/src/obse/test.js
+++ vh4/src/obse/test.js
@@ -1,11 +1,29 @@
 const expect = require('expect')
 const Obse = require('./')
+const mockQueue = require('@badgrhammer/rabbitmq-helpers/src/mock')
 
+let obse
 describe('===== OBSE =====', () => {
+
+  beforeEach(() => {
+    obse = new Obse()
+  })
+
   describe('==== constructor ====', () => {
     it('Should create a new instance without errors', async() => {
       expect(() => new Obse()).not.toThrow()
-    })  
+    })
+    it('Should set default state to be RUNNING', async() => {
+      expect(obse.state).toEqual('RUNNING')
+    })    
   })
+  
+  describe('==== initQueue ====', () => {
+    it('Should return mock queue implementation in test env', () => {
+      obse.initQueue()
+      expect(obse.queue).toMatchObject(mockQueue)
+    })
+  })
+
 
 })
